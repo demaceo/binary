8ee169bdbc6837c3afe60c964a408fc262b1d171
Demaceo Howard <hdemaceo@gmail.com>
2021-01-14 12:28:55 -0700

    Create Header component

---
+++ src/Icons/MenuIcons.js
@@ -0,0 +1,141 @@
+// export default settings = (
+//   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 80" x="0px" y="0px">
+//     <g>
+//       <path d="M32,22A10,10,0,1,0,42,32,10.0111,10.0111,0,0,0,32,22Zm0,16a6,6,0,1,1,6-6A6.0066,6.0066,0,0,1,32,38Zm21.7822-3.07a19.8131,19.8131,0,0,0,0-5.8594l5.2608-4.623a1,1,0,0,0,.206-1.251l-6-10.3926a1.0013,1.0013,0,0,0-1.1865-.4473l-6.6157,2.2378a21.9422,21.9422,0,0,0-5.0938-2.9277L38.9805,4.8037A1,1,0,0,0,38,4H26a1,1,0,0,0-.98.8037L23.647,11.6665a21.9422,21.9422,0,0,0-5.0938,2.9277l-6.6157-2.2378a1.0019,1.0019,0,0,0-1.1865.4473l-6,10.3926a1,1,0,0,0,.206,1.251l5.2608,4.623a19.8131,19.8131,0,0,0,0,5.8594L4.957,39.5527a1,1,0,0,0-.206,1.251l6,10.3926a1,1,0,0,0,1.1865.4473l6.6157-2.2383a21.9288,21.9288,0,0,0,5.0938,2.9282L25.02,59.1963A1,1,0,0,0,26,60H38a1,1,0,0,0,.98-.8037l1.3725-6.8628a21.9288,21.9288,0,0,0,5.0938-2.9282l6.6157,2.2383a1,1,0,0,0,1.1865-.4473l6-10.3926a1,1,0,0,0-.206-1.251ZM32,44A12,12,0,1,1,44,32,12.0137,12.0137,0,0,1,32,44Z" />
+//     </g>
+//   </svg>
+// );
+
+const home = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    xlink="http://www.w3.org/1999/xlink"
+    version="1.1"
+    x="0px"
+    y="0px"
+    viewBox="0 0 100 125"
+    // style="enable-background:new 0 0 100 100;"
+    // xml:space="preserve"
+  >
+    <g>
+      <path d="M41.3,5H5v36.3h36.3V5z M39.3,39.3H7V7h32.3V39.3z" />
+      <path d="M58.7,41.3H95V5H58.7V41.3z M60.7,7H93v32.3H60.7V7z" />
+      <path d="M41.3,59H5v36.3h36.3V59z M39.3,93.3H7V61h32.3V93.3z" />
+      <path d="M58.7,95.3H95V59H58.7V95.3z M60.7,61H93v32.3H60.7V61z" />
+    </g>
+  </svg>
+);
+
+export default home;
+
+// export default hamburger = (
+//   <svg
+//     xmlns:dc="http://purl.org/dc/elements/1.1/"
+//     xmlns:cc="http://creativecommons.org/ns#"
+//     xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+//     xmlns:svg="http://www.w3.org/2000/svg"
+//     xmlns="http://www.w3.org/2000/svg"
+//     xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd"
+//     xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape"
+//     version="1.1"
+//     x="0px"
+//     y="0px"
+//     viewBox="0 0 100 125"
+//   >
+//     <g transform="translate(0,-952.36218)">
+//       <path
+//         style="text-indent:0;text-transform:none;direction:ltr;block-progression:tb;baseline-shift:baseline;color:#000000;enable-background:accumulate;"
+//         d="m 8.0000002,970.36217 0,12 83.9999998,0 0,-12 z m 0,26 0,12.00003 83.9999998,0 0,-12.00003 z m 0,26.00003 0,12 83.9999998,0 0,-12 z"
+//         fill="#000000"
+//         fill-opacity="1"
+//         stroke="none"
+//         marker="none"
+//         visibility="visible"
+//         display="inline"
+//         overflow="visible"
+//       />
+//     </g>
+//   </svg>
+// );
+
+// export default plus = (
+//   <svg
+//     xmlns:dc="http://purl.org/dc/elements/1.1/"
+//     xmlns:cc="http://creativecommons.org/ns#"
+//     xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+//     xmlns:svg="http://www.w3.org/2000/svg"
+//     xmlns="http://www.w3.org/2000/svg"
+//     xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd"
+//     xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape"
+//     version="1.1"
+//     x="0px"
+//     y="0px"
+//     viewBox="0 0 100 125"
+//   >
+//     <g transform="translate(0,-952.36218)">
+//       <path
+//         d="m 50,978.3622 c -2.2092,0 -4,1.79089 -4,4 l 0,16 -16,0 c -2.2091,0 -4,1.7908 -4,4 0,2.2092 1.7909,4 4,4 l 16,0 0,16 c 0,2.2091 1.7908,4 4,4 2.2092,0 4,-1.7909 4,-4 l 0,-16 16,0 c 2.2091,0 4,-1.7908 4,-4 0,-2.2092 -1.7909,-4 -4,-4 l -16,0 0,-16 c 0,-2.20911 -1.7908,-4 -4,-4 z"
+//         style="text-indent:0;text-transform:none;direction:ltr;block-progression:tb;baseline-shift:baseline;color:#000000;enable-background:accumulate;"
+//         fill="#000000"
+//         fill-opacity="1"
+//         stroke="none"
+//         marker="none"
+//         visibility="visible"
+//         display="inline"
+//         overflow="visible"
+//       />
+//     </g>
+//   </svg>
+// );
+
+
+// export default gridIcon = (
+//   <svg
+//     xmlns="http://www.w3.org/2000/svg"
+//     xlink="http://www.w3.org/1999/xlink"
+//     version="1.1"
+//     x="0px"
+//     y="0px"
+//     viewBox="0 0 64 80"
+//     enable-background="new 0 0 64 64"
+//     space="preserve"
+//   >
+//     <path d="M23.713,37.898H4.452c-1.319,0-2.389,1.069-2.389,2.389v19.262c0,1.319,1.07,2.389,2.389,2.389h19.261  c1.319,0,2.389-1.069,2.389-2.389V40.287C26.102,38.968,25.032,37.898,23.713,37.898z" />
+//     <path d="M23.713,2.063H4.452c-1.319,0-2.389,1.07-2.389,2.389v19.261c0,1.319,1.07,2.389,2.389,2.389h19.261  c1.319,0,2.389-1.07,2.389-2.389V4.452C26.102,3.132,25.032,2.063,23.713,2.063z" />
+//     <path d="M59.549,37.898H40.287c-1.319,0-2.39,1.069-2.39,2.389v19.262c0,1.319,1.07,2.389,2.39,2.389h19.262  c1.319,0,2.389-1.069,2.389-2.389V40.287C61.938,38.968,60.868,37.898,59.549,37.898z" />
+//     <path d="M59.549,2.063H40.287c-1.319,0-2.39,1.07-2.39,2.389v19.261c0,1.319,1.07,2.389,2.39,2.389h19.262  c1.319,0,2.389-1.07,2.389-2.389V4.452C61.938,3.132,60.868,2.063,59.549,2.063z" />
+
+//   </svg>
+// );
+
+
+// export default homeIcon = (
+//   <svg
+//     xmlns:dc="http://purl.org/dc/elements/1.1/"
+//     xmlns:cc="http://creativecommons.org/ns#"
+//     xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+//     xmlns:svg="http://www.w3.org/2000/svg"
+//     xmlns="http://www.w3.org/2000/svg"
+//     xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd"
+//     xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape"
+//     version="1.1"
+//     x="0px"
+//     y="0px"
+//     viewBox="0 0 100 125"
+//   >
+//     <g transform="translate(0,-952.36218)">
+//       <path
+//         style="text-indent:0;text-transform:none;direction:ltr;block-progression:tb;baseline-shift:baseline;color:#000000;enable-background:accumulate;"
+//         d="m 49.946122,970.3624 c -0.41963,0.009 -0.81114,0.14803 -1.1875,0.40625 l -38,30.00005 c -0.8742804,0.7021 -0.9983304,1.9978 -0.34375,2.8437 0.64517,0.8338 2.01747,0.9672 2.84375,0.3125 l 36.75,-29 36.75,29 c 0.82628,0.6547 2.16085,0.5162 2.8125,-0.3125 0.64676,-0.8225 0.57722,-2.1888 -0.3125,-2.8437 l -38,-30.00005 c -0.44511,-0.29323 -0.89287,-0.415 -1.3125,-0.40625 z m -24.9375,26.90625 -4,3.15625 0,31.9375 a 2.0001999,2.0001999 0 0 0 2,2 l 19,0 a 2.0001999,2.0001999 0 0 0 2,-2 l 0,-12 c 0,-3.3586 2.64141,-6 6,-6 3.35859,0 6,2.6414 6,6 l 0,12 a 2.0001999,2.0001999 0 0 0 2,2 l 19,0 a 2.0001999,2.0001999 0 0 0 2,-2 l 0,-31.9375 -4,-3.15625 0,33.09375 -15,0 0,-10 c 0,-5.5054 -4.49459,-10 -10,-10 -5.50541,0 -10,4.4946 -10,10 l 0,10 -15,0 0,-33.09375 z"
+//         fill="#000000"
+//         fill-opacity="1"
+//         stroke="none"
+//         marker="none"
+//         visibility="visible"
+//         display="inline"
+//         overflow="visible"
+//       />
+//     </g>
+//   </svg>
+// );
+
