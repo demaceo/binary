c48200a8579748ad15963e5415ea3e3eba3608f2
Demaceo Howard <hdemaceo@gmail.com>
2021-01-15 14:07:29 -0700

    Remove commented out code

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,75 +1,34 @@
 import "./NavBar.css";
 import { useState, useRef, useEffect } from "react";
-import { solidGridIcon } from '../../Icons/MenuIcons.js';
+// import { solidGridIcon } from '../../Icons/MenuIcons.js';
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { Overlay, Button } from "react-bootstrap";
+import AddToDo from '../AddToDo/AddToDo';
 
 function NavBar() {
-  const bubbleRef = useRef();
-  const tabRef = useRef();
-  const tabsRef = useRef();
+    const [show, setShow] = useState(false);
+    const [target, setTarget] = useState(null);
+    //const ref = useRef(null);
 
-    // const mouseOver = (event) => {
-    //   event.target.classList.add('hover');
-    //   return {
-    //     left: event.traget.offsetLeft + "px"
-    //   };
-    // };
-
-    // const mouseLeave = () => {
-    //     return {
-    //     transition: 'transform 200ms ease-out',
-    //     transform:'scale(1,0) translateX(-25%)'
-    //     }
-    // };
-
-    // const mouseOut = (event) => {
-    //     event.target.classList.remove("hover")
-    // };
-
-    // const mouseEnter = (event) => {
-    //         setTimeout(
-    //             () =>
-    //             ( {
-    //                 transition: "transform 200ms ease-out, left 300ms ease-out"
-    //             }),
-    //             5
-    //         );
-    //         return {
-    //             transform: "scale(1,1) translateX(-25%)"
-    //         }
-    // }
-
+    const handleClick = (event) => {
+        setShow(!show);
+        setTarget(event.target);
+        return <AddToDo />
+    };
   return (
     <div className="nav-container">
-      <div className="bubble-container">
-        <svg
-
-          className="bubble"
-        //   onMouseOver={mouseOver}
-        //   onMouseEnter={mouseEnter}
-        //   onMouseLeave={mouseLeave}
-        //   onMouseOut={mouseOut}
-          xmlns="http://www.w2.org/2000/svg"
-          viewBox="0 0 493.99 107.9"
-          preserveAspectRatio="none"
-        >
-          <path
-            className="bubble-path"
-            d="M492.24,104.4c-34.4,0-67.6-2.55-98.76-18.51C342.17,59.61,329.28,14.94,270,2.79h0A141.58,141.58,0,0,0,244.75,0a141.58,141.68,0,0,0-25.23,2.78h-.05C160.22,14.94,147.33,59.61,96,85.89a172.18,172.18,0,0,1-49.16,16.37c-15,2.57-30,2.14-45.11,2.14a1.75,1.75,0,0,0,0,3.5H492.24a1.75,1.75,0,0,0,0-3.5Z"
-          />
-        </svg>
-      </div>
-      <ul className="tabs">
-        <li className="tab">
-          <span className="icon icon1">{solidGridIcon}</span>
-          <label>Home</label>
+      <ul className="nav-bar">
+        <li className="option">
+          <i className="fa fa-home" aria-hidden="true"></i>
+          <span className="option-text">Home</span>
         </li>
-        <li className="tab">
-          <span className="icon icon2">+</span>
-          <label>Add</label>
+        <li className="option" onClick={handleClick}>
+          <i className="fa fa-plus" aria-hidden="true"></i>
+          <span className="option-text">Add</span>
         </li>
-        <li className="tab" >
-          <span className="icon icon3">{solidGridIcon}</span>
-          <label>To-Dos</label>
+        <li className="option">
+          <i className="fa fa-th-large" aria-hidden="true"></i>
+          <span className="option-text">To-Dos</span>
         </li>
       </ul>
     </div>
