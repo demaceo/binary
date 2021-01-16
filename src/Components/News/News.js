abc0c383ed692aa8b2654f1e658d44cec9cfd79b
Demaceo Howard <hdemaceo@gmail.com>
2021-01-16 14:47:44 -0700

    Display News articles on Home page

--- src/Components/News/News.js
+++ src/Components/News/News.js
@@ -1,53 +1,37 @@
 import "./News.css";
 import { useState, useEffect } from "react";
-import { Carousel } from "react-bootstrap";
+import Article from '../Article/Article';
+import { getBreakingNews } from "../../utilities/apiCalls";
 
-function News() {
-    const [news, setNews] = useState();
-    const [article, setArticle] = useState();
-    const [index, setIndex] = useState(0);
-    const handleSelect = (selectedIndex, e) => {
-    setIndex(selectedIndex);
-    };
-    return (
-      <div className="news-container">
-        <Carousel>
-        <Carousel.Item>
-            <img
-              className="d-block w-100"
-              src="https://specials-images.forbesimg.com/imageserve/5e6e2adfaa5428000759e994/960x0.jpg?fit=scale"
-              alt="First slide"
-            />
-            {/* <Carousel.Caption>
-              <h3>First slide label</h3>
-                <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-            </Carousel.Caption> */}
-        </Carousel.Item>
-        <Carousel.Item>
-            <img
-              className="d-block w-50"
-              src="https://cdn.cnn.com/cnnnext/dam/assets/200415000852-08-sony-world-photography-awards.jpg"
-              alt="Second slide"
-            />
-            {/* <Carousel.Caption>
-              <h3>Second slide label</h3>
-                <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-            </Carousel.Caption> */}
-        </Carousel.Item>
-        <Carousel.Item>
-            <img
-              className="d-block w-100"
-              src="https://cdn.cnn.com/cnnnext/dam/assets/200415000520-05-sony-world-photography-awards.jpg"
-              alt="Third slide"
-            />
-            {/* <Carousel.Caption>
-              <h3>Third slide label</h3>
-                <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-            </Carousel.Caption> */}
-        </Carousel.Item>
-        </Carousel>
-   </div>
-    );
+const News = () => {
+    const [breakingNews, setBreakingNews] = useState([]);
+    useEffect( async() =>  {
+        await getBreakingNews().then((data) => setBreakingNews(data));
+      }, []);
+
+    const newsArticles = breakingNews.map((article) => {
+        let title = article.title.rendered.replaceAll("&#8217;", "'");
+        title = title.replace( /(<([^>]+)>)/ig, '');
+        let content = article.content.rendered.replace(/(<([^>]+)>)/gi, "");
+        content = content.replaceAll("&#8217;", "'");
+        content = content.replaceAll("&#8230;", "");
+        content = content.replaceAll('Read More', "")
+        return (
+          <Article
+            key={article.id}
+            id={article.id}
+            title={title}
+            content={content}
+          />
+        );
+      });
+    console.log(breakingNews)
+  return (
+    <section className="news-container">
+      <h2 className="news-title">News</h2>
+      {newsArticles}
+    </section>
+  );
 }
 
 export default News;
