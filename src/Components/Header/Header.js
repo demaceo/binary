a2d9a869ea2e559bb2c1bb9a7abfb73aae384205
Demaceo Howard <hdemaceo@gmail.com>
2021-01-15 21:07:02 -0700

    Refactor counter ref

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -6,50 +6,56 @@ function Header() {
     const [time, setTime] = useState();
     const [date, setDate] = useState(Date().toLocaleString().slice(0, 11));
     const [counter, setCounter] =  useState(0);
+    const renderCounter = useRef(0)
     const currentMonth = date.slice(0, 7);
     const timeRef = useRef();
     const hourGlassRef = useRef();
+    const dateRef = useRef();
     
     useEffect(()  => {
         setInterval(() => {
             setTime(new Date().toLocaleString().slice(11))
         }, 1000)
-    }, []);
+        renderCounter.current = renderCounter.current + 1
+    }, [])
     
-    const toggleTime = () => {
-        if (!counter) {
-            timeRef.current.classList.remove('hidden')
-            hourGlassRef.current.classList.add('hidden');
+    const toggleTime = async(day) => {
+        console.log("dateref", dateRef.current.innerText);
+        if (!renderCounter.current) {
+          timeRef.current.classList.remove("hidden");
+          hourGlassRef.current.classList.add("hidden");
         } else {
-            timeRef.current.classList.add('hidden');
-            hourGlassRef.current.classList.remove("hidden");
+          timeRef.current.classList.add("hidden");
+          hourGlassRef.current.classList.remove("hidden");
         }
     }
 
-    const rewind = () => {
+    const rewind = async() => {
         let newDayNum = (Number(date.slice(8)) - 1).toString();
         let previousDay = currentMonth.concat(" " + newDayNum);
         if(counter !== -7) {
-            setCounter(counter-1);   
             setDate(previousDay);
-            toggleTime();
+            renderCounter.current = renderCounter.current - 1; 
+            toggleTime(date); 
         }
     }
 
-  const fastForward = () => {
+  const fastForward = async() => {
     let newDayNum = (Number(date.slice(8)) + 1).toString();
     let nextDay = currentMonth.concat(" " + newDayNum);
     if (counter !== 7) {
-        setCounter(counter+1);
         setDate(nextDay);
-        toggleTime();
+        renderCounter.current = renderCounter.current + 1;
+        toggleTime(date);
+        
+        
     } 
   };
 
     return (
       <div className="header-container">
-        <h2 className="date">{date}</h2>
-        <h3 className="time" ref={timeRef}>{time}</h3>
+        <h2 className="date" ref={ dateRef }>{date}</h2>
+        <h3 className="time" ref={ timeRef }>{time}</h3>
         <i class="fas fa-hourglass-half hidden" ref={hourGlassRef} />
         <div className="header-arrows">
           <i className="fas fa-chevron-left" id="left" onClick={rewind}></i>
