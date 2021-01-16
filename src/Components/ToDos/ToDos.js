d88daa46d4a64f46ba89db2300293132941d1fa6
Demaceo Howard <hdemaceo@gmail.com>
2021-01-15 19:12:17 -0700

    Create and add functionality to ToDos component

--- src/Components/ToDos/ToDos.js
+++ src/Components/ToDos/ToDos.js
@@ -1,10 +1,38 @@
 import React from 'react'
 import AddToDo from '../AddToDo/AddToDo';
+import './ToDos.css';
 
 export default function ToDos() {
     return (
-        <div>
-            
+      <div className="priorities-container center">
+        <div className="priority">
+          <div className="box" style={{ backgroundColor: "#3C7FF2" }}>I</div>
+          <div className="content center">
+            <h1>Important & Urgent</h1>
+            <p></p>
+          </div>
         </div>
-    )
+        <div className="priority">
+          <div className="box" style={{ backgroundColor: "#FBA260" }}></div>
+          <div className="content center">
+            <h1>Important, Not Urgent</h1>
+            <p></p>
+          </div>
+        </div>
+        <div className="priority">
+          <div className="box" style={{ backgroundColor: "#8A78F0" }}></div>
+          <div className="content center">
+            <h1>Urgent, Not Important</h1>
+            <p></p>
+          </div>
+        </div>
+        <div className="priority">
+          <div className="box" style={{ backgroundColor: "#EE6C74" }}></div>
+          <div className="content center">
+            <h1>Not Important, Not Urgent</h1>
+            <p></p>
+          </div>
+        </div>
+      </div>
+    );
 }
