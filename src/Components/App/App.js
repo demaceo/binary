56c9bcbc3d411e3c0b4f7c9267e7a6f2954cc47b
Demaceo Howard <hdemaceo@gmail.com>
2021-01-15 19:11:27 -0700

    Create addToDo function

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -3,27 +3,36 @@ import { useState, useEffect } from 'react';
 import NavBar from '../NavBar/NavBar';
 import AddToDo from '../AddToDo/AddToDo';
 import LogIn from '../LogIn/LogIn';
+import Header from '../Header/Header';
+import ToDos from '../ToDos/ToDos';
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  console.log(todos);
+  
+  const addToDo = (newToDo) => {
+    if (todos.length === 0) {
+      setTodos([newToDo]);
+      // setLocalStorage(toDos);
+    } 
+    // else if (!todos.includes(newToDo)) {
+    //   setTodos([newToDo, ...todos]);
+    //   // setLocalStorage([newToDo, ...todos])
+    // }
+      console.log(todos);
+  }
+  
+  const delToDo = (id) => {
+    const filteredToDos = todos.filter((todo) => todo.id !== id);
+    setTodos(filteredToDos); 
+  }
+
   return (
     <div className="App">
-      < AddToDo />
-      {/* <LogIn /> */}
-      < NavBar />
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
+      <Header />
+      <ToDos />
+      <AddToDo addToDo={addToDo} />
+      <NavBar />
     </div>
   );
 }
