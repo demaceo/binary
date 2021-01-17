720e9d68b661ad215cfb9e55ecd0f9c8eceb62c9
Demaceo Howard <hdemaceo@gmail.com>
2021-01-16 17:45:30 -0700

    Remove unwanted characters within articles

--- src/Components/News/News.js
+++ src/Components/News/News.js
@@ -5,32 +5,46 @@ import { getBreakingNews } from "../../utilities/apiCalls";
 
 const News = () => {
     const [breakingNews, setBreakingNews] = useState([]);
+
     useEffect( async() =>  {
         await getBreakingNews().then((data) => setBreakingNews(data));
       }, []);
 
     const newsArticles = breakingNews.map((article) => {
+
         let title = article.title.rendered.replaceAll("&#8217;", "'");
         title = title.replace( /(<([^>]+)>)/ig, '');
+        title = title.replaceAll("F&#038;F9", "");
         let content = article.content.rendered.replace(/(<([^>]+)>)/gi, "");
         content = content.replaceAll("&#8217;", "'");
         content = content.replaceAll("&#8230;", "");
-        content = content.replaceAll('Read More', "")
+        content = content.replaceAll("F&#038;F9", "");
+        content = content.replaceAll("&#8220;", "");
+        content = content.replaceAll("&#8221;", "");
+        content = content.replace('Read More', "");
+
+        if (article.jetpack_featured_media_url !== "") {
+
         return (
           <Article
             key={article.id}
             id={article.id}
+            image={article.jetpack_featured_media_url}
             title={title}
             content={content}
+            link={article.link}
           />
         );
+        }
       });
-    console.log(breakingNews)
+
   return (
-    <section className="news-container">
-      <h2 className="news-title">News</h2>
-      {newsArticles}
-    </section>
+      <>
+        <h2 className="news-title">News</h2>
+        <section className="news-container">
+            {newsArticles}
+        </section>
+    </>
   );
 }
 
