59d9feba0232b88b15b52cbaab07fe8dda6017f1
Demaceo Howard <hdemaceo@gmail.com>
2021-01-17 19:11:49 -0700

    Create individualized Links for each Category

--- src/Components/ToDos/ToDos.js
+++ src/Components/ToDos/ToDos.js
@@ -1,43 +1,62 @@
 import React from 'react'
 import './ToDos.css';
+import { useState, useEffect, useRef } from "react";
+import { Link, BrowserRouter as Router, Route } from "react-router-dom";
 
-export default function ToDos() {
+function ToDos() {
+    let category = "Important & Urgent";
+    let categoryRef = useRef('');
+   const updateCategory = (cat) => {
+        category = cat;
+    }
     return (
       <div className="priorities-container center">
-        <div className="priority">
-          <div className="box center" style={{ backgroundColor: "#3C7FF2" }}>
-
-          </div>
+        <Link to={`/category/${category}`} className="priority">
+          <div
+            className="box center"
+            style={{ backgroundColor: "#3C7FF2" }}
+            onMouseOver={updateCategory("Important & Urgent")}
+          ></div>
           <div className="content center">
             <h1>Important & Urgent</h1>
-            <p></p>
           </div>
-        </div>
-        <div className="priority">
-          <div className="box center" style={{ backgroundColor: "#FBA260" }}>
-
-          </div>
+        </Link>
+        <Link to={`/category/${category}`} className="priority">
+          <div
+            className="box center"
+            style={{ backgroundColor: "#FBA260" }}
+            onMouseOver={updateCategory("Important, Not Urgent")}
+          ></div>
           <div className="content center">
             <h1>Important, Not Urgent</h1>
-            <p></p>
           </div>
-        </div>
-        <div className="priority">
-          <div className="box center" style={{ backgroundColor: "#8A78F0" }}>
-          </div>
+        </Link>
+        <Link
+          to={`/category/${category}`}
+          className="priority"
+        >
+          <div
+            className="box center"
+            style={{ backgroundColor: "#8A78F0" }}
+            onMouseOver={updateCategory("Urgent, Not Important")}
+          ></div>
           <div className="content center">
             <h1>Urgent, Not Important</h1>
-            <p></p>
           </div>
-        </div>
-        <div className="priority">
-          <div className="box center" style={{ backgroundColor: "#EE6C74" }}>
-          </div>
+        </Link>
+        <Link to={`/category/${category}`} className="priority">
+          <div
+            className="box center"
+            style={{ backgroundColor: "#EE6C74" }}
+            onMouseOver={updateCategory("Not Important Nor Urgent")}
+         
+          ></div>
           <div className="content center">
             <h1>Not Important Nor Urgent</h1>
-            <p></p>
           </div>
-        </div>
+        </Link>
       </div>
     );
-}
+};
+
+export default ToDos;
